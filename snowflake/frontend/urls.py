5964fb66814052e1da79419540ec4112e380a0e0
Jack Moxon <jsmoxon@gmail.com>
2012-01-04 13:58:13 -0800

    fixes

--- snowflake/frontend/urls.py
+++ snowflake/frontend/urls.py
@@ -4,6 +4,4 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'frontend.views.home', name='home'),
-    url(r'test/', 'frontend.views.home'),
-    # url(r'^snowflake/', include('snowflake.foo.urls')),
 )
